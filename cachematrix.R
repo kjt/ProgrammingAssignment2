ee12e7c34997942901f525475ba99e97003675a0
Keith Thoresz <thorek1@gmail.com>
2014-11-17 22:46:01 -0500

    First cut at matrix cache solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
+packageTest <- function(x)
+{
+  if (!require(x,character.only = TRUE))
+  {
+    install.packages(x,dep=TRUE)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
+
+packageTest('matrixcalc')
+
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  if (class(x) != "matrix") {
+    stop ("Argument must be a square matrix")
+  }
 
+  d = dim(x)
+  if (d[1] != d[2]) {
+    stop ("Matrix must be square")
+  }
+  
+  if (is.singular.matrix(x)) {
+    stop ("Matrix is singular and cannot be inverted")
+  }
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
